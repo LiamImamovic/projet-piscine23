0b47128f5c546e23ca93e15885e087e8e87f2af8
liamimamovic <liamimamovic1@gmail.com>
2023-05-23 18:54:49 +0200

    add some styles

--- api/index.js
+++ api/index.js
@@ -11,12 +11,12 @@ app.use(express.json());
 app.use(cookieParser());
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
+  destination: (req, file, cb) => {
     cb(null, "../client/public/upload");
   },
-  filename: function (req, file, cb) {
+  filename: (req, file, cb) => {
     cb(null, Date.now() + file.originalname);
-  },
+  }
 });
 
 const upload = multer({ storage });
